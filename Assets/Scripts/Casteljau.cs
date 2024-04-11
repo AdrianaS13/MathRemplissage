9b613e8408bea90410bfdf152c1fb40926309316
Sarra Koussaier <95942823+sarrakou@users.noreply.github.com>
2024-04-11 09:13:57 +0200

    adding algorithm

--- Assets/Scripts/Casteljau.cs
+++ Assets/Scripts/Casteljau.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Casteljau : MonoBehaviour
 {
+    [SerializeField] private int k;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,19 @@ public class Casteljau : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void deCasteljau(List<Vector2Int> originalPolygonPoints)
+    {
+        for (int t = 0; t < 1; t+=(1/k))
+        {
+            for (int j=0; j< originalPolygonPoints.Count; j++)
+            {
+                for (int i=0; i< originalPolygonPoints.Count-j; i++)
+                {
+                    originalPolygonPoints[j] = (1 - t) * originalPolygonPoints[i] + t * originalPolygonPoints[i+1];
+                }
+            }
+        }
     }
 }
